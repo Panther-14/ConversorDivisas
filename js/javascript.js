e66856c59f69a94989d50d39c37d8ca041cf0107
Panther-14 <lego.ninjago.city@gmail.com>
2024-05-16 11:45:30 -0600

    Bug fix

--- js/javascript.js
+++ js/javascript.js
@@ -41,22 +41,15 @@ function convertir(){
 
     request.onload = function() {
         if(request.status >= 200 && request.status < 300) {
-            //Convierte la respuesta del WS a un objeto JSON
             var data = JSON.parse(this.response);
-            //Imprime en consola el objeto JSON resultante
             console.log(data);
-            //Asigna a la variable var_rates solo el arreglo de divisas contenido en la respuesta del WS
             var var_rates = data.rates;
-            //Se extrae la tasa de conversión a Pesos Mexicanos de los resultados de WS
             var tasa = parseFloat(var_rates['MXN']);
-                //Se valida que el importe y la tasa no sea Nulo y sea mayor a cero
                 if(!isNaN(importe) && importe > 0.0 && !isNaN(tasa) && tasa > 0.0){
                     txtasa.value = tasa;
-                    //Si es de Pesos a Dolares
                     if(pesos_a_dolares){
                         res.value = (importe / tasa);
                     }
-                    //Si es de Dolares a Pesos
                     else {
                         res.value = (importe * tasa);
                     }
@@ -68,5 +61,5 @@ function convertir(){
 
     request.send();
 
-    return false; //Evita que se ejecute el redirect del FORM
+    return false;
 }
